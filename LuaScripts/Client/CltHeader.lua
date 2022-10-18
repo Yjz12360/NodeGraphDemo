bbef2a3aa5158ef4a30967bc6e6a81028d3a166b
yaojianzhao <yaojianzhao@corp.netease.com>
2022-10-18 15:11:23 +0800

    延迟节点接定时器模块

--- LuaScripts/Client/CltHeader.lua
+++ LuaScripts/Client/CltHeader.lua
@@ -1,6 +1,7 @@
+rawRequire "Client/PrintMod"
 
 require "Client/CltGameMod"
 require "Client/CltNodeGraphMod"
-require "Client/PrintMod"
+
 
 
--- LuaScripts/Public/NodeGraph/Nodes/DelayNode.lua
+++ LuaScripts/Public/NodeGraph/Nodes/DelayNode.lua
@@ -1,26 +1,12 @@
 
 function CltHandler(tNodeGraph, tNodeData)
-    local nTimer = 0
-    function fUpdate(nDeltaTime)
-        nTimer = nTimer + nDeltaTime
-        if nTimer >= tNodeData.nDelayTime then
-            CltNodeGraphMod.finishNode(tNodeGraph, tNodeData.sNodeId)
-        end
-    end
-    return {
-        fUpdate = fUpdate,
-    }
+    TimerMod.delay(tNodeData.nDelayTime, function()
+        CltNodeGraphMod.finishNode(tNodeGraph, tNodeData.sNodeId)
+    end)
 end
 
 function SvrHandler(tNodeGraph, tNodeData)
-    local nTimer = 0
-    function fUpdate(nDeltaTime)
-        nTimer = nTimer + nDeltaTime
-        if nTimer >= tNodeData.nDelayTime then
-            SvrNodeGraphMod.finishNode(tNodeGraph, tNodeData.sNodeId)
-        end
-    end
-    return {
-        fUpdate = fUpdate,
-    }
+    TimerMod.delay(tNodeData.nDelayTime, function()
+        SvrNodeGraphMod.finishNode(tNodeGraph, tNodeData.sNodeId)
+    end)
 end
